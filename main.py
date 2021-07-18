56a25b46759a204058f23eb687bac779a41bfb10
Bionicallianc3 <oreoluwaakinbo.oa@gmail.com>
2021-07-18 15:36:31 +0100

    Main Module

---
+++ main.py
@@ -0,0 +1,47 @@
+fruits = ['apple', 'banana'
+    ,'cherry']
+for fruit in fruits:
+    if fruit == 'banana':
+        continue
+    print(fruit)
+
+
+x = 4
+print(x % 7+6)
+print(20**3)
+
+j = 0
+while j<=5:
+    if j ==3:
+        print(j)
+    j += 1
+
+for m in list(range(9)):
+    if m==5:
+        continue
+    print(m)
+
+''''print('Telles' in teamA)
+print('shaw' in teamA)
+print('luke' in teamA)
+print(teamA)
+index = teamA.index('Marcus')
+print('The index of Marcus:', index)
+index = teamA.index('shaw')
+pass
+print(index)'''
+
+#help(sorted)
+
+age = 65
+txt= ' I am Horey, aged ' + str(age) + ' years'
+print(txt)
+
+print(txt.strip())
+print(txt.isprintable())
+
+
+
+
+
+
